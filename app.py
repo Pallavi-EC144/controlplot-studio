0f7094704de8715e882949eb112eca9816ab2a1e
Pallavi-EC144 <pallavikv459@gmail.com>
2026-06-28 09:26:27 +0530

    Refactor imports in app.py for consistency

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
-import scipy as sp
 from scipy import signal
-import sympy as sym
+import sympy as sp
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import json
 import math
 
 # Set page config
